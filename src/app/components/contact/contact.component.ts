a87909e48441c7c6ebc65420014db8495f635024
Luis Gustavo <gustavodasilvajustino@hotmail.com>
2024-08-06 19:50:32 -0300

    feat: Implement logic for contact component

--- src/app/components/contact/contact.component.ts
+++ src/app/components/contact/contact.component.ts
@@ -1,17 +1,22 @@
-import { AfterViewInit, Component, ElementRef } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, inject, Input, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from '@angular/forms';
 
 import { ScrollAnimationService } from '../../services/scroll-animation.service';
 import { FooterComponent } from '../layout/footer/footer.component';
 import { SidebarComponent } from './../layout/sidebar/sidebar.component';
+import { ContactService } from '../../services/contact.service';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [SidebarComponent, FooterComponent],
+  imports: [SidebarComponent, FooterComponent, FormsModule, ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent implements AfterViewInit {
+export class ContactComponent implements OnInit, AfterViewInit {
+  @Input("formGroup") FormData!: FormGroup;
+  builder = inject(FormBuilder);
+  contactService = inject(ContactService);
 
   constructor(
     private el: ElementRef,
@@ -22,5 +27,29 @@ export class ContactComponent implements AfterViewInit {
     const elements = this.el.nativeElement.querySelectorAll('.form-container > *');
     this.scrollAnimationService.observeElements(Array.from(elements), 'animated');
   }
+
+  ngOnInit() {
+    this.FormData = this.builder.group({
+      Fullname: new FormControl('', [Validators.required]),
+      Email: new FormControl('', [Validators.required, Validators.email]),
+      Message: new FormControl('', [Validators.required])
+    });
+  }
+
+  onSubmit() {
+    if (this.FormData.valid) {
+      this.contactService.PostMessage(this.FormData.value).subscribe(
+        response => {
+          location.href = 'https://mailthis.to/confirm';
+          console.log(response);
+        },
+        error => {
+          console.warn(error.responseText);
+          console.log({ error });
+        }
+      );
+    }
+  }
+
 
 }
